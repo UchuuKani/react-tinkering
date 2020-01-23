3a38a5253c0745107b2c0540aa4c00b254c9f6ef
Alex Y <probesalesman@gmail.com>
2020-01-23 16:19:28 -0500

    adds RightSlot component

--- src/RightSlot.js
+++ src/RightSlot.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function RightSlot(props) {
   return (
-  <div>
-    <h2>This is the RightSlot</h2>
-  </div>
+    <div>
+      <h2>This is the RightSlot</h2>
+    </div>
   );
 }
 
